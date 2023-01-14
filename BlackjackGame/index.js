6f46e3ee9b7d565bca4bd8b626ea45f0e2f4c732
ElahiAli <ali_elahi00@outlook.com>
2023-01-14 22:24:23 +0330

    working on BlackJack game

--- BlackjackGame/index.js
+++ BlackjackGame/index.js
@@ -1,7 +1,25 @@
-let min = 2;
-let max = 21;
-let firstCard = 5;
-let secondCard = 8;
-
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
-console.log(sum);
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardEl = document.getElementById("card-el");
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw another cart?";
+  } else if (sum === 21) {
+    message = "Wooohooo! You got BlackJack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Card: " + " " + firstCard + " , " + secondCard;
+}
