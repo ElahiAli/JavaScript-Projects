dae3cd468bd09aac60c58b8823c8e47ee9000683
ElahiAli <ali_elahi00@outlook.com>
2023-01-15 20:35:56 +0330

    [add]BlackJack: adding new cards button.

--- BlackjackGame/index.js
+++ BlackjackGame/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10;
 let secondCard = 11;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -10,6 +11,10 @@ let sumEl = document.querySelector("#sum-el");
 let cardEl = document.getElementById("card-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
   if (sum <= 20) {
     message = "Do you want to draw another cart?";
   } else if (sum === 21) {
@@ -21,5 +26,15 @@ function startGame() {
   }
   messageEl.textContent = message;
   sumEl.textContent = "Sum: " + sum;
-  cardEl.textContent = "Card: " + " " + firstCard + " , " + secondCard;
+  cardEl.textContent = "Cards: ";
+  for (let card = 0; card < cards.length; card++) {
+    cardEl.textContent += cards[card] + " ";
+  }
+}
+
+function newCard() {
+  let thirdCard = 5;
+  cards.push(thirdCard);
+  sum += thirdCard;
+  renderGame();
 }
