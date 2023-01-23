eebc38290bd362b2a88bb78c92793a0447a51ae8
ElahiAli <ali_elahi00@outlook.com>
2023-01-23 21:14:27 +0330

    knowing innerHTML

--- ChromeExtenssion/index.js
+++ ChromeExtenssion/index.js
@@ -1,5 +1,13 @@
-let inputBtn = document.getElementById("input-btn");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked here!");
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+for (let i = 0; i <= myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
