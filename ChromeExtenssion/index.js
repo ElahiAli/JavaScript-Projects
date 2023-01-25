c9dca0040e2e6b36f2fcdb31e5414e9941da925b
ElahiAli <ali_elahi00@outlook.com>
2023-01-25 19:55:48 +0330

    learn how to clear input field

--- ChromeExtenssion/index.js
+++ ChromeExtenssion/index.js
@@ -5,14 +5,20 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  inputEl.value = "";
+  renderLeads();
 });
 
-for (let i = 0; i <= myLeads.length; i++) {
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderLeads() {
+  let listItem = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItem += "<li>" + myLeads[i] + "</li>";
 
-  // other way:
-  // const li = document.createElement("li")
-  // li.textContent = myLeads[i]
-  // ulEl.append(li)
+    // other way:
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+  }
+
+  ulEl.innerHTML = listItem; //innerHTML should be out of the loop for better performance.
 }
