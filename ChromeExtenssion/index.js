84ccde749dc8d98813b72a76b58a5feab9d10fb5
ElahiAli <ali_elahi00@outlook.com>
2023-01-30 20:55:59 +0330

    dblclick add this for double click!

--- ChromeExtenssion/index.js
+++ ChromeExtenssion/index.js
@@ -4,7 +4,7 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
+let deleteBtn = document.getElementById("delete-btn");
 
 // working with localStorage:
 // localStorage.setItem(key,value)
@@ -17,7 +17,7 @@ if (leadFromLocalStorage) {
   renderLeads();
 }
 
-deleteBtn.addEventListener("dbclick", function () {
+deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
   renderLeads();
