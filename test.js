4057b18de90b1e7d97ed1d286a952e2c65683c4d
ElahiAli <ali_elahi00@outlook.com>
2023-01-30 19:21:11 +0330

    turning string to array and reverse, localStorage

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
-const recipient = "james";
-let sender = "Ali";
-const email = `
-    Hey ${recipient}! How is it going? Cheers ${sender} ok
-    Bye!
- `;
-console.log(email);
+// const recipient = "james";
+// let sender = "Ali";
+// const email = `
+//     Hey ${recipient}! How is it going? Cheers ${sender} ok
+//     Bye!
+//  `;
+// console.log(email);
+
+//turning the string to array:
+// let myLeads = `["www.awesomelead.com"]`;
+
+// myLeads = JSON.parse(myLeads);
+
+// myLeads.push("www.example.com");
+// console.log(myLeads);
+
+//turning array to string:
+// let myLeads = ["www.awesomelead.com"];
+// myLeads.push("www.example.com");
+// myLeads = JSON.stringify(myLeads);
+// console.log(typeof myLeads);
