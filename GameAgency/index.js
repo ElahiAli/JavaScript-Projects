3e07051ac40bc1645da19fd80854208797c8ebec
ElahiAli <ali_elahi00@outlook.com>
2023-02-01 19:53:05 +0330

    index.js

---
+++ GameAgency/index.js
@@ -0,0 +1,13 @@
+let devBtn = document.getElementById("container");
+
+const images = ["images/img1.jpg", "images/img2.jpg", "images/img3.png"];
+
+function imagesList(imageList) {
+  let listImage = "";
+  for (i = 0; i <= imageList.length; i++) {
+    listImage += `<img class="team-image" src="${imageList[i]}">`;
+  }
+  devBtn.innerHTML = listImage;
+}
+
+imagesList(images);
