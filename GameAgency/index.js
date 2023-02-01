1468de752c21e8a675684069069854bd0de44d41
ElahiAli <ali_elahi00@outlook.com>
2023-02-01 20:03:52 +0330

    change length in js file

--- GameAgency/index.js
+++ GameAgency/index.js
@@ -4,8 +4,8 @@ const images = ["images/img1.jpg", "images/img2.jpg", "images/img3.png"];
 
 function imagesList(imageList) {
   let listImage = "";
-  for (i = 0; i <= imageList.length; i++) {
-    listImage += `<img class="team-image" src="${imageList[i]}">`;
+  for (i = 0; i < imageList.length; i++) {
+    listImage += `<img alt="Game Image" class="team-image" src="${imageList[i]}">`;
   }
   devBtn.innerHTML = listImage;
 }
