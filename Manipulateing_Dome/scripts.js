71f8f34e2248711266cda5d38e22d7f583b482dc
ElahiAli <ali_elahi00@outlook.com>
2023-02-27 13:34:23 +0330

    manipulation

--- Manipulateing_Dome/scripts.js
+++ Manipulateing_Dome/scripts.js
@@ -1,27 +1,30 @@
-// const body = document.body;
-// body.append("hello");
+const body = document.body;
+body.append("hello");
 
 // const body = document.body;
-// const div = document.createElement("div");
-// div.innerText = "Hello world";
-// body.append(div);
+const div = document.createElement("div");
+div.innerText = "Hello world";
+body.append(div);
+// body.appendChild()
 
-//creating element and give them value
+// creating element and give them value
 // const body = document.body;
-// const strong = document.createElement("strong");
-// strong.innerText = "Hello world";
-// body.append(strong);
+const strong = document.createElement("strong");
+strong.innerText = "Hello world";
+body.append(strong);
 
-//remove element
-// const div = document.querySelector("div")
-// div.remove()
+// remove element
+const div = document.querySelector("div");
+div.remove();
 
-//accessing the element inside of HTML
-// console.log(div.getAttribute("id")) ; div.title ; div.id  //div already has access to the attrbute.
-// div.setAttrbute("id","value")  // adding element to div with it's value, it can be use for updating an attrbute
-// div.removeAttrbute("id") // removing attrbute from element
+// accessing the element inside of HTML
+console.log(div.getAttribute("id"));
+div.title;
+div.id; //div already has access to the attrbute.
+div.setAttrbute("id", "value"); // adding element to div with it's value, it can be use for updating an attrbute
+div.removeAttrbute("id"); // removing attrbute from element
 
-// classList.toggle("hi3", true or false) // remove the attrbute if it already exist or add if does'nt exists
+classList.toggle("hi3", "true or false"); // remove the attrbute if it already exist or add if does'nt exists
 
 // change style
-// div.style.backgroundColor = 'red'
+div.style.backgroundColor = "red";
