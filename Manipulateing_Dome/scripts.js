f281ebaef0e4f0802605600f53cd188a0aa860a5
ElahiAli <ali_elahi00@outlook.com>
2023-02-27 09:06:42 +0330

    Manipulation Dom

---
+++ Manipulateing_Dome/scripts.js
@@ -0,0 +1,27 @@
+// const body = document.body;
+// body.append("hello");
+
+// const body = document.body;
+// const div = document.createElement("div");
+// div.innerText = "Hello world";
+// body.append(div);
+
+//creating element and give them value
+// const body = document.body;
+// const strong = document.createElement("strong");
+// strong.innerText = "Hello world";
+// body.append(strong);
+
+//remove element
+// const div = document.querySelector("div")
+// div.remove()
+
+//accessing the element inside of HTML
+// console.log(div.getAttribute("id")) ; div.title ; div.id  //div already has access to the attrbute.
+// div.setAttrbute("id","value")  // adding element to div with it's value, it can be use for updating an attrbute
+// div.removeAttrbute("id") // removing attrbute from element
+
+// classList.toggle("hi3", true or false) // remove the attrbute if it already exist or add if does'nt exists
+
+// change style
+// div.style.backgroundColor = 'red'
