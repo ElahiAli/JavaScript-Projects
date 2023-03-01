6dee636ce6e3c6d269606d5188ff825aea1ef63a
ElahiAli <ali_elahi00@outlook.com>
2023-03-01 14:39:14 +0330

    render function

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,1 +1,36 @@
 import "./style.css";
+import { getWeather } from "./weather";
+import { ICON_MAP } from "./iconMaps";
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
+  .then(renderWeather)
+  .catch((e) => {
+    console.error(e);
+    alert("Error getting weather.");
+  });
+
+function renderWeather({ current, daily, hourly }) {
+  renderCurrentWeather(current);
+  //   renderDailyWeather();
+  //   renderHourlyWeather();
+  document.body.classList.remove("blurred");
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function getIconUrl(iconCode) {
+  return `./icon/${ICON_MAP.get(iconCode)}.svg`;
+}
+const currentIcon = document.querySelector("[data-current-icon]");
+function renderCurrentWeather(current) {
+  currentIcon.src = getIconUrl(current.iconCode);
+  setValue("current-temp", current.currentTemp);
+  setValue("current-high", current.highTemp);
+  setValue("current-low", current.lowTemp);
+  setValue("current-fl-high", current.highFeelsLike);
+  setValue("current-fl-low", current.lowFeelsLike);
+  setValue("current-wind", current.windSpeed);
+  setValue("current-precip", current.precip);
+}
