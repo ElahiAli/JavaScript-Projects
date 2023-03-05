4645a63ac6ccd24292df04849d678bda9c8d1a9e
ElahiAli <ali_elahi00@outlook.com>
2023-03-05 09:42:45 +0330

    query and params

--- NODEJS/start_express/express.js
+++ NODEJS/start_express/express.js
@@ -5,4 +5,10 @@ exp.get("/", (req, res) => {
   res.send("hello world");
 });
 
+exp.get("/example/:name/:age", (req, res) => {
+  res.send(req.params.name + " : " + req.params.age);
+  console.log(req.params);
+  console.log(req.query); // :3000/example/:name/:age?tutorial=paramsTutorial&sort=byage
+});
+
 exp.listen("3000");
